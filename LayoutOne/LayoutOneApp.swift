8060c71ebd6e9e01bef9847ae7188354cf47b876
Sam <sbekmuratov2@students.wiut.uz>
2025-05-02 16:21:51 +0500

    Initial Commit

---
+++ LayoutOne/LayoutOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutOneApp.swift
+//  LayoutOne
+//
+//  Created by Sukhrob on 02/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
